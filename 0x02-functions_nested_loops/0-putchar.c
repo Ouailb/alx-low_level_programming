3efd88f7ece5c974f3b1c7f629219f4636af876b
ouailb <adomeyou@gmail.com>
2023-06-20 00:16:30 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(x[i]);
+	}
+
+		_putchar('\n');
+
+	return (0);
+}
+
