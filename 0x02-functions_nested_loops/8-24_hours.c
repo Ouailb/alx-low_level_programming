c514ed5dc5dad2e8661d4256e5fa4454a1b3fb5f
ouailb <adomeyou@gmail.com>
2023-06-20 05:17:13 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+
+/**
+ * jack_bauer - Check Main
+ * Description: function prints every minute in one day
+ * Return: Nothing
+ */
+
+	void jack_bauer(void)
+	{
+
+		int hours, minutes;
+
+
+		for (hours = 0; hours < 24; hours++)
+		{
+			for (minutes = 0; minutes < 60; minutes++)
+			{
+
+				_putchar('0' + hours / 10);
+				_putchar('0' + hours % 10);
+
+				_putchar(':');
+
+				_putchar('0' + minutes / 10);
+				_putchar('0' + minutes % 10);
+
+				_putchar('\n');
+
+			}
+		}
+
+	}
+
