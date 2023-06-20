8a500d80f8fefc6b36a788788f41208404b92780
ouailb <adomeyou@gmail.com>
2023-06-20 09:05:08 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Check Main
+ * @x: An integer x
+ * @y: An integer y
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
