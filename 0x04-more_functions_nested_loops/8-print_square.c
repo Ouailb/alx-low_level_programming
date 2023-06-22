3d1aa389b8e7dbde2e547dbe649442429e8dca35
ouailb <adomeyou@gmail.com>
2023-06-22 14:32:26 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
+
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		for (j = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
