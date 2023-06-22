72d8469bf580ed4565505639bbfc0c3f70e99a55
ouailb <adomeyou@gmail.com>
2023-06-22 08:21:13 -0700

    task 5 edit 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -21,8 +21,8 @@ void more_numbers(void)
 		for (x = 0; x <= 14; x++)
 		{
 			if (x >= 10)
-				_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
+				_putchar((x / 10) + 48);
+			_putchar((x % 10) + 48);
 
 		}
 		_putchar('\n');
