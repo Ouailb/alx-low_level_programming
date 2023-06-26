c62b59a27992c10e415cca797132b934f9fc8103
ouailb <adomeyou@gmail.com>
2023-06-26 17:00:16 +0100

    task 6 edit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,16 @@
  */
 void puts2(char *str)
 {
-    int x = 0;
+	int x = 0;
 
-    while (str[x] != '\0')
-    {
-        if (x % 2 == 0)
-        {
-            _putchar(str[x]);
-        }
-        x++;
-    }
+	while (str[x] != '\0')
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+		x++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
