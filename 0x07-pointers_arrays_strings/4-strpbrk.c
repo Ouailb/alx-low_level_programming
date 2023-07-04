beef27e5278e7cfe6ae3132b5ef311c613b80948
ouailb <adomeyou@gmail.com>
2023-07-04 10:32:35 +0100

    task 4 edit 8

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,18 +6,20 @@
  * Return: a pointer to the byte in s that matches one of the
  * bytes in accept, or NULL if no such byte is found.
  */
+
+
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j;
 
 	while (*(s + i) != '\0')
 	{
-		while (*(accept + j) != '\0')
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				return (s + i);
-			j++;
 		}
+
 		i++;
 	}
 	return ('\0');
