33b991a69ba18048d5cfbbfa07072e7f37bb19c6
ouailb <adomeyou@gmail.com>
2023-07-04 09:51:52 +0100

    task 4 edit 2

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,19 +7,17 @@
  * bytes in accept, or NULL if no such byte is found.
  */
 
-
 char *_strpbrk(char *s, char *accept)
 {
-	for (; *s != '\0'; s++)
-	{
-		char *a = accept;
+	unsigned int i, j;
 
-		for (; *a != '\0'; a++)
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*s == *a)
-				return (s);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
 	}
-
-	return (NULL);
+	return ('\0');
 }
