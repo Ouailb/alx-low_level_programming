ac847731f56c34c1ea7c22d5746775b6f872b7e3
ouailb <adomeyou@gmail.com>
2023-07-07 23:34:36 +0100

    task 0 edit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,8 @@
  * Return: 0 - success.
  */
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", argv[0]);
-    return (0);
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
 }
