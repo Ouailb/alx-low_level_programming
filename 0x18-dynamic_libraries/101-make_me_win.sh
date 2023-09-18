57adb6d397f6ff515969a2fe03dcd1f424536dc9
ouailb <adomeyou@gmail.com>
2023-09-18 22:39:23 +0100

    101+++++++

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/MubarekSD/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
+wget -P ../ https://github.com/Ouailb/alx-low_level_programming/blob/master/0x18-dynamic_libraries/firstoy.so
 export LD_PRELOAD=../libhack.so
