f30bcb2112b856bda71323b524b175e6395f4c49
ouailb <adomeyou@gmail.com>
2023-09-18 14:24:53 +0100

    101+++

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Ouailb/alx-low_level_programming/ \
+wget -P /tmp/ https://raw.github.com/Ouailb/alx-low_level_programming/ \
         tree/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
